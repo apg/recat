edc689a46d2235c6d2584bc98e3cf3a4a2f84396
Andrew Gwozdziewycz <git@apgwoz.com>
2013-03-20 11:16:24 -0400

    * make a release on pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'recat',
+    version = '0.1',
+    packages = [],
+    author = 'Andrew Gwozdziewycz',
+    author_email = 'web@apgwoz.com',
+    description = 'Replay log files simply and easily',
+    license = 'GPLv3',
+    keywords = 'log replay',
+    url = 'https://github.com/apgwoz/recat',
+    scripts = ['recat']
+)
